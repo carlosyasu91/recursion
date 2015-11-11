9b72c367279720b0a6fc19fd445d2bf90c1ea3f1
Yasunari Flores <carlosyasu91@gmail.com>
2015-11-11 16:50:11 -0700

    Finished recursion functions

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,20 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className, node){
+	
+	var result = [];
+	//create a default value for the node
+	node = node || document.body;
+	//create a variable to store the results
+	var parts = node.className.split(' ');
+	if(parts.indexOf(className) >= 0){
+		result.push(node);
+	}
+	for(var i=0; i<node.children.length;i++){
+		//for each child, search for elements with the target classname
+		var childResults = getElementsByClassName(className, node.children[i]);
+		result  = result.concat(childResults);
+	}
+	return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,48 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
   // your code goes here
+  var current = '';
+  var first = true;
+  if(obj === undefined && typeof obj === 'function'){
+    return ;
+  }
+  if(obj === null){
+    return 'null';
+  } else if(typeof obj === 'string'){
+    return '"' + obj.toString() + '"'
+  } else if(obj.constructor === Array){
+     var result = '[';
+     for(var i=0;i<obj.length;i++){
+      if(typeof obj[i] !== 'function' && obj[i] !== undefined){
+      result += stringifyJSON(obj[i]);
+      if(i<obj.length-1){
+       result+=',';
+      }
+    }
+     }
+     result+= ']';
+     return result;
+  } 
+  else if(typeof obj !== 'object'){
+    return obj.toString();
+  } else {
+  for(var prop in obj){    
+    if(typeof obj[prop] !== 'function' && obj[prop] !== undefined){
+    if(!first){
+      current += ','
+    }
+    if(typeof obj[prop] !== 'object' && obj[prop] === null){
+      current+= '"' + prop + '"' +':'+ stringifyJSON(obj[prop]);
+    } else {    
+      current += '"' + prop + '"' + ':' + stringifyJSON(obj[prop]);
+    }
+
+      first = false;
+    }
+  }
+
+  }
+  return  '{' + current + '}';
 };
